563c579ebf0876c0ee4474a0efa0747957c0b37f
Porfonya <137227248+Porfonya@users.noreply.github.com>
2024-06-18 22:52:43 +0300

    master branch

---
+++ src/main/java/com/example/websiteAT/WebsiteAtApplication.java
@@ -0,0 +1,13 @@
+package com.example.websiteAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteAtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websiteAT/WebsiteAtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websiteAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteAtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
